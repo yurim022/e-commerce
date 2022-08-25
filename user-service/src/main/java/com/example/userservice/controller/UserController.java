57086799ba720d4f811891caaa65c16d39eba608
yurim022 <yurim2220@gmail.com>
2022-08-25 20:09:33 +0900

    feat user service - use property

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,2 +1,29 @@
-package com.example.userservice.controller;public class UserController {
+package com.example.userservice.controller;
+
+import com.example.userservice.vo.Greeting;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final Environment env;
+    private final Greeting greeting;
+
+    @GetMapping("/health-check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
 }
