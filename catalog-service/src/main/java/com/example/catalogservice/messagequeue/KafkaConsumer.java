a1cb4c6fb421330280dedec8fa378f5f6d233230
10151688 <yurim2220@gmail.com>
2022-10-24 10:17:23 +0900

    feat Catalog-Service register kafka consumer

---
+++ catalog-service/src/main/java/com/example/catalogservice/messagequeue/KafkaConsumer.java
@@ -0,0 +1,46 @@
+package com.example.catalogservice.messagequeue;
+
+import com.example.catalogservice.entity.CatalogEntity;
+import com.example.catalogservice.jpa.CatalogRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Service
+public class KafkaConsumer {
+
+    CatalogRepository repository;
+
+    @Autowired
+    public KafkaConsumer(CatalogRepository repository) {
+        this.repository = repository;
+    }
+
+    /* order service 에서 주문하면 재고 줄도록 topic consume */
+    @KafkaListener(topics = "example-catalog-topic")
+    public void updateQty(String kafkaMessage) {
+        log.info("Kafka Message: -> " + kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {});
+        } catch(JsonProcessingException exception){
+            exception.printStackTrace();
+        }
+
+        CatalogEntity entity = repository.findByProductId((String) map.get("productId"));
+        if (entity != null) {
+            entity.setStock(entity.getStock() - (Integer) map.get("qty"));
+            repository.save(entity);
+        }
+    }
+}
