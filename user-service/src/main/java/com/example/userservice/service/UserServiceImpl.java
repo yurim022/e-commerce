7261e9a0b23aa49388ae846430bd478aee039574
10151688 <yurim2220@gmail.com>
2022-10-29 00:13:25 +0900

    feat zipkin & sleuth 적용 및 테스트

--- user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -110,9 +110,12 @@ public class UserServiceImpl implements UserService {
         /* ErrorDecoder */
 //        List<ResponseOrder> orders = orderServiceClient.getOrders(userId);
 
+        log.info("Before call orders microservice");
         CircuitBreaker circuitbreaker = circuitBreakerFactory.create("circuitbreaker");
         List<ResponseOrder> orderList = circuitbreaker.run(() -> orderServiceClient.getOrders(userId),
                 throwable -> new ArrayList<>());
+        log.info("After call orders microservice");
+
         userDto.setOrders(orderList);
         return userDto;
     }
